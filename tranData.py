db924a25b8463b99472a09950d4dc5425345e7e6
AwsomeName <liuchang.hit.cs@gmail.com>
2019-05-12 18:04:37 +0800

    update, bugs fixed

--- tranData.py
+++ tranData.py
@@ -29,10 +29,12 @@ print(' remove useless lines in articles ')
 for articles in tqdm(allArticles[0]):
     flag = True
     for element in articles:
-        if element.startswith('<doc id') or element.startswith('</doc>'):
+        if element.startswith('<doc id') or element.startswith('</doc>') or element.startswith('<doc'):
             flag = False
     if flag :
         text.append(articles)
+
+del allArticles
 
 # remain only word and numbers
 def findAllTokens(string):
@@ -40,7 +42,7 @@ def findAllTokens(string):
 
 print(' remain word and numbers ')
 textTokens = [findAllTokens(x) for x in tqdm(text)]
-
+# print(text[:100])
 del text
 
 # change to simplified
@@ -53,20 +55,27 @@ print(' tokenization ')
 def cut(string): return list(jieba.cut(string))
 allTokens = [cut(x) for x in tqdm(textTokens)]
 
+# print(textTokens[:100])
 del textTokens
 
 # get off space
 print (' get off space ')
-validTokens = [ x for x in tqdm(allTokens)  for t in x if t.strip()]
+validTokens = [[t for t in x if t.strip() ]
+               for x in tqdm(allTokens) if len(x)>0 and x[0]!='doc']
 
+# print(allTokens[:100])
 del allTokens
 
+# print(validTokens[:100])
 # join list to sentence
 print( 'generate sentences ')
-sentences = [' '.join(x) for x in tqdm(validTokens)]
+#sentences = [' '.join(x) for x in tqdm(validTokens)]
 
 with open('wiki_sentences.txt', 'w') as f:
-    f.writelines([sentence+'\n' for sentence in sentences])
+    for x in tqdm(validTokens):
+        sentence = ' '.join(x)
+        f.write(sentence+'\n')
+        #f.writelines([sentence+'\n' for sentence in sentences])
 
 
 
--- tranEmb_Gensim2.0.py
+++ tranEmb_Gensim2.0.py
@@ -1,6 +1,6 @@
 import gensim
 import logging
-
+from gensim.models.word2vec import LineSentence
 
 paths = ['wiki_sentences.txt']
 sizes = [100]
@@ -15,14 +15,8 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 for path in paths:
     for size in sizes:
-        with open(path, "r", encoding='utf8') as f:
-            sentences = f.readlines()
-            model = gensim.models.Word2Vec(sentences, min_count=10, size=size)
-            modelFile = path.split('.')[0] + '.' + str(size)
-            print(modelFile)
-            model.save(modelFile)
-
-
-
-
-
+        sentences = LineSentence(path)
+        model = gensim.models.Word2Vec(sentences, min_count=10, size=size)
+        modelFile = path.split('.')[0] + '_' + str(size)
+        print(modelFile)
+        model.save(modelFile)
